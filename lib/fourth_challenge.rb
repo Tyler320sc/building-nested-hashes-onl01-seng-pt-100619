1e92d43c5e5b512729782793fa511efaae009e1c
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-22 02:03:50 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,7 +7,10 @@ def fourth_challenge
       :hero_friends => [
         {name: "Benvolio",
         age: "17",
-        attitude: "confused"}
+        attitude: "worried"}
+        {name: "Mercutio",
+        age: "18",
+        attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
